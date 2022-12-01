429e6a67404a6000ad4cec422b281a814393f112
petrichor-hl <tl.hoanglam174@gmail.com>
2022-12-02 02:10:10 +0700

    add some logic

--- Model/Lam/InputInfo.cs
+++ Model/Lam/InputInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,13 @@ namespace ConvenienceStore.Model.Lam
     {
         public int Id { get; set; }
         public DateTime InputDate { get; set; }
+        public int UserId { get; set; }
         public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public byte[] Avatar { get; set; }
+        public int SupplerId { get; set; }
         public string SupplierName { get; set; }
-
         public List<Product> products { get; set; }
 
         public InputInfo()
