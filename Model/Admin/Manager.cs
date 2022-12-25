a5a501b294bbb94877ef402273c2a4305404c531
21521252 <21521252@gm.uit.edu.vn>
2022-12-25 13:47:37 +0700

    ok

--- Model/Admin/Manager.cs
+++ Model/Admin/Manager.cs
@@ -10,10 +10,9 @@ namespace ConvenienceStore.Model.Admin
     {
         public int Id { get; set; }
         public string Name { get; set; }
-public string Address { get; set; }
+        public string Address { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-
         public override string ToString()
         {
             return Name;
--- Model/Admin/SalaryBill.cs
+++ Model/Admin/SalaryBill.cs
@@ -1,31 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConvenienceStore.Model.Admin
+﻿namespace ConvenienceStore.Model.Admin
 {
     public class SalaryBill
     {
         private int id;
         public int IdBill { get => id; set => id = value; }
-
         private int idAccount;
         public int IdAccount { get => idAccount; set => idAccount = value; }
-
         private string billDate;
         public string BillDate { get => billDate; set => billDate = value; }
-
-
         private int customerId_;
         public int CustomerId { get => customerId_; set => customerId_ = value; }
-
         private long price;
         public long Price { get => price; set => price = value; }
-
         private string employeeName_;
         public string EmployeeName { get => employeeName_; set => employeeName_ = value; }
-
     }
 }
