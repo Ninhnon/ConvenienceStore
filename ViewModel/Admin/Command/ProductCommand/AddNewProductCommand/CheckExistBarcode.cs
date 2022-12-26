fc692004f77e0c854a2cfa2195770bd0e92e87fa
petrichor-hl <21520318@gm.uit.edu.vn>
2022-12-26 15:40:51 +0700

    add more validation

---
+++ ViewModel/Admin/Command/ProductCommand/AddNewProductCommand/CheckExistBarcode.cs
@@ -0,0 +1,51 @@
+﻿using ConvenienceStore.ViewModel.Admin.AdminVM;
+using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ConvenienceStore.ViewModel.Admin.Command.ProductCommand.AddNewProductCommand
+{
+    public class CheckExistBarcode : ICommand
+    {
+        InputInfoVM VM;
+
+        public CheckExistBarcode(InputInfoVM VM)
+        {
+            this.VM = VM;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            var values = (object[])parameter;
+            var s = (string)values[0];
+            return 8 <= s.Length && s.Length <= 13 ;
+        }
+
+        public void Execute(object parameter)
+        {
+            var values = (object[])parameter;
+            var s = (string)values[0];
+            var barcodeErrorMessage = (TextBlock)values[1];
+            for (int i = 0; i < VM.SelectedInputInfo.products.Count; ++i)
+            {
+                if (s == VM.SelectedInputInfo.products[i].Barcode)
+                {
+                    barcodeErrorMessage.Text = "Mã sản phẩm này đã được nhập";
+                    return;
+                }
+            }
+            barcodeErrorMessage.Text = "";
+        }
+    }
+}
